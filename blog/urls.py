5d5c9df64d0c237de03da5744761231dabdc1313
Mr.BoO <56259930+Master-MonSter@users.noreply.github.com>
2023-10-16 20:32:35 +0330

    Create a new app
    +Create a blog app
    +Customize its pages
    (blog-home, blog-single.html)
    +Some edit base.html for blog's pages

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.index_view, name='index'),
+    path('single', views.single_blog, name='single')
+]
--- my_site/urls.py
+++ my_site/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('website.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls'))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

51cccf47375bad9ae379ce00db1571e2fbb12b5c
Mr.BoO <56259930+Master-MonSter@users.noreply.github.com>
2023-10-21 21:54:41 +0330

    Start DB
    +Create a db for blog(Post)
    +Create a db for contact(Contact)
    +(all things are models & admin of app)

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from website.models import Contact
+
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_display = ('name', 'subject', 'email', 'created_date')
+    list_filter = ('email',)
+    # It's prefer to Meta class
+    # ordering = ('-created_date',) 
+    search_fields = ('name', 'email')
 
 # Register your models here.
+admin.site.register(Contact, ContactAdmin)
--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from blog.models import Post
+
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title', 'status', 'counted_views', 'created_date', 'published_date')
+    list_filter = ('status',)
+    # It's prefer to Meta class
+    # ordering = ('-created_date',) 
+    search_fields = ('title', 'content')
+
+admin.site.register(Post, PostAdmin)

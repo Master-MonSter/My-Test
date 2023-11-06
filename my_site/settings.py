49b8e0051b00596fe7c901cd5268f0f02370a217
Mr.BoO <56259930+Master-MonSter@users.noreply.github.com>
2023-11-06 14:17:34 +0330

    Simple Captcha
    + Install Simple Captcha for forms
    
    + Register captcha in website\forms.py
    
    + Add Captcha to website\contact.html

--- my_site/settings.py
+++ my_site/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.sitemaps',
     'django_summernote',
+    'captcha',
     'robots',
     'taggit',
     'debug_toolbar',
--- website/forms.py
+++ website/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+from captcha.fields import CaptchaField
 from website.models import Contact, NewsLetter
 
 class ContactForm(forms.ModelForm):
-
+    captcha = CaptchaField()
     class Meta:
         model = Contact
         exclude = ('subject',)
